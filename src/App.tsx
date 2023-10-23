1662f125efcf053f5e667db46292dd72119ecbfd
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2023-10-24 01:23:27 +0300

    feat: basic routing for two pages and wrong route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Route, Routes, useNavigate } from 'react-router';
+import { useAppSelector } from './app/hooks';
+import LoginPage from './pages/LoginPage';
+import Footer from './components/Footer';
+import TablePage from './pages/TablePage';
+import ErrorMessage from './components/ErrorMessage';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const navigate = useNavigate();
+  const { token } = useAppSelector((state) => state.account);
+
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  }, [token]);
+  return (<div className='container'>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/' element={<TablePage />} />
+      <Route path='*' element={
+        <main className="main main__welcome">
+        <ErrorMessage error={new Error('404 not found')}/>
+      </main>} />
+    </Routes>
+    <Footer />
+  </div>);
 }
 
 export default App;
